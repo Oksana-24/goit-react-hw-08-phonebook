9ecff2375b9ed09ed3d8363ffb96aa2a4309e22c
Oksana Khymych <66249893+Oksana-24@users.noreply.github.com>
2023-08-06 13:40:17 +0200

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,13 @@
-import React from 'react';
-
-const Home = () => {
-  return <div>Home</div>;
-};
-
-export default Home;
+export default function Home() {
+  return (
+    <section>
+      <h2>Your smart contact book!</h2>
+      <h2> The free and easy way to collect phone numbers.</h2>
+      <h2>
+        The best tool to help you collect the phone numbers from friends and
+        family celebration , party invitations or other events.
+      </h2>
+      <p>Convenient filtering for quick search.</p>
+    </section>
+  );
+}
