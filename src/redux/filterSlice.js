ae7387656b9b0bd8a4e5b17acf41241c9f5c3d22
Oksana Khymych <66249893+Oksana-24@users.noreply.github.com>
2023-08-06 13:40:34 +0200

    Update filterSlice.js

---
+++ src/redux/filterSlice.js
@@ -0,0 +1,23 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const filterInitialState = {
+  filter: '',
+};
+
+const filterSlice = createSlice({
+  name: 'filter',
+  initialState: filterInitialState,
+
+  reducers: {
+    filterContact(state, { payload }) {
+      return {
+        ...state,
+        filter: payload,
+      };
+    },
+  },
+});
+
+export const { filterContact } = filterSlice.actions;
+
+export const filterReducer = filterSlice.reducer;
