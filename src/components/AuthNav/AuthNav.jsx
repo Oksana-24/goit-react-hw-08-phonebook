c810a25be9001651e6e705754f4a2612587f5a2e
Oksana Khymych <66249893+Oksana-24@users.noreply.github.com>
2023-08-06 13:39:40 +0200

    Update AuthNav.jsx

--- src/components/AuthNav/AuthNav.jsx
+++ src/components/AuthNav/AuthNav.jsx
@@ -1,5 +1,10 @@
-import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 export const AuthNav = () => {
-  return <div>AuthNav</div>;
+  return (
+    <div>
+      <NavLink to="/register">Register</NavLink>
+      <NavLink to="/login">Log In</NavLink>
+    </div>
+  );
 };
