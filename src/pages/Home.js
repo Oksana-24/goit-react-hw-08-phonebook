41f0008bf017582b721f051f4e5a2c7e55ac1fad
Oksana Khymych <66249893+Oksana-24@users.noreply.github.com>
2023-08-06 14:12:08 +0200

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,7 @@
 export default function Home() {
   return (
     <section>
-      <h2>Your smart contact book!</h2>
-      <h2> The free and easy way to collect phone numbers.</h2>
-      <h2>
-        The best tool to help you collect the phone numbers from friends and
-        family celebration , party invitations or other events.
-      </h2>
-      <p>Convenient filtering for quick search.</p>
+      <h2>Your contact book!</h2>
     </section>
   );
 }
