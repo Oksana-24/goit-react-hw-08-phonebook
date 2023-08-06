05ca941a12aa48cb9d4654c27b6db6127aa942f6
Oksana Khymych <66249893+Oksana-24@users.noreply.github.com>
2023-08-06 13:40:21 +0200

    Update Register.js

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import { Helmet } from 'react-helmet';
+import { RegisterForm } from 'components/RegisterForm/RegisterForm';
 
-const Register = () => {
-  return <div>Register</div>;
-};
-
-export default Register;
+export default function Register() {
+  return (
+    <div>
+      <Helmet>
+        <title>Registration</title>
+      </Helmet>
+      <RegisterForm />
+    </div>
+  );
+}
