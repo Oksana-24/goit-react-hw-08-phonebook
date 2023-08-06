c36db0701576fefc3690e1423ff3ed483dc67941
Oksana Khymych <66249893+Oksana-24@users.noreply.github.com>
2023-08-06 13:40:19 +0200

    Update Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import { Helmet } from 'react-helmet';
+import { LoginForm } from 'components/LoginForm/LoginForm';
 
-const Login = () => {
-  return <div>Login</div>;
-};
-
-export default Login;
+export default function Login() {
+  return (
+    <div>
+      <Helmet>
+        <title>Login</title>
+      </Helmet>
+      <LoginForm />
+    </div>
+  );
+}
