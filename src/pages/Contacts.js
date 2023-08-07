2a3eff0506392b71aae932260160a66f1cb6aae2
Oksana Khymych <66249893+Oksana-24@users.noreply.github.com>
2023-08-07 17:35:43 +0200

    Update Contacts.js

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -1,13 +1,21 @@
 import ContactList from '../components/ContactList/ContactList';
 import Filter from '../components/Filter/Filter';
 import { Loader } from '../components/Loader/Loader';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { selectLoading } from 'redux/contacts/selectors';
 import { Helmet } from 'react-helmet';
 import { Form } from 'components/Form/Form';
+import { useEffect } from 'react';
+import { fetchContacts } from 'redux/contacts/operations';
 
 export function Contacts() {
   const isLoading = useSelector(selectLoading);
+  const dispatch = useDispatch();
+  // const isLoading = useSelector(selectLoading);
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
 
   return (
     <div>
